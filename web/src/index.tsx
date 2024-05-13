7fcd15e6b74042d8d06e509e9b9c897c292f46d7
Yamboy1 <yamboyd1@gmail.com>
2024-05-13 20:05:01 +1200

    Remove web stuff

--- web/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import { start } from "./server";
-
-// Start up the Bun server
-// Configure port and development mode via your .env file (see README)
-start();
--- web/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    colors: false,
-  },
-  plugins: [require("daisyui")],
-};
