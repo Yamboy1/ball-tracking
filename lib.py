8e6acb15c19f9ea2f437781cb049046e3c4d5aa3
Yamboy1 <yamboyd1@gmail.com>
2024-06-13 18:02:48 +1200

    Replace erosion with opening

--- lib.py
+++ lib.py
@@ -8,17 +8,16 @@ def runBackgroundSubtractor(frame, kernelSize):
     fgMask = backSub.apply(blurFrame)
     # Filter out shadows. The background subtractor detects shadows
     # and displays them with a lower value in the grayscale image.
-    # 230 is a value that appears to work here, but no guarantees.
+    # Shadows are anything below 255, so we set the threshold to be 254.
     _, threshold = cv.threshold(fgMask, 254, 255, cv.THRESH_BINARY)
-    # Not sure why I'm doing this, maybe it's useful, maybe i want to erode it instead?
-    # See https://www.geeksforgeeks.org/erosion-dilation-images-using-opencv-python/
-    # for more information on dilation and erosion :)
-    #dilated = cv.dilate(threshold, cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3)), iterations=2)
-    eroded = cv.erode(threshold, None, iterations=2)
-    dilated = eroded
-    contours, _ = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # With some trial and error, it looks like a circular kernel of size 4
+    # seems to work best, along with an opening with 3 iterations. 4 iterations
+    # was cutting out slightly more frames of the ball than I would have liked.
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(4,4))
+    opening = cv.morphologyEx(threshold, cv.MORPH_OPEN, kernel, iterations=3)
+
+    contours, _ = cv.findContours(opening, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     cv.drawContours(frame, contours, -1, (0,0,255), 3)
     mask = cv.cvtColor(fgMask,cv.COLOR_GRAY2BGR)
-    predilateMask = cv.cvtColor(threshold,cv.COLOR_GRAY2BGR)
-    thresholdMask = cv.cvtColor(dilated,cv.COLOR_GRAY2BGR)
-    return np.concatenate((frame, mask, predilateMask, thresholdMask), axis=1)
+    openingMask = cv.cvtColor(opening, cv.COLOR_GRAY2BGR)
+    return np.concatenate((frame, openingMask), axis=1)
